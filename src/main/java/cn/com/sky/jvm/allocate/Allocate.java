c365c832d4e1b72371260f9c3df668b30b3b1dcc
git-sky <linkme2008@sina.com>
2017-07-31 14:48:10 +0800

    add

--- src/main/java/cn/com/sky/jvm/allocate/Allocate.java
+++ src/main/java/cn/com/sky/jvm/allocate/Allocate.java
@@ -13,6 +13,17 @@ package cn.com.sky.jvm.allocate;
  * 
  * 整个JVM内存大小=年轻代大小 + 老年代大小 + 持久代大小。
  * 
+ * 
+ * DefNewGeneration是default new generation 
+ * ParNewGeneration是parallel new generation 
+ * 
+ * DefNew：是使用-XX:+UseSerialGC（新生代，老年代都使用串行回收收集器）。
+ * ParNew：是使用-XX:+UseParNewGC（新生代使用并行收集器，老年代使用串行回收收集器）
+ *       -XX:+UseConcMarkSweepGC(新生代使用并行收集器，老年代使用CMS)。
+ * 
+ * PSYoungGen: -XX:+UseParallelGC（新生代使用并行回收收集器，老年代使用串行收集器）
+ * -XX:+UseParallelOldGC（新生代，老年代都使用并行回收收集器）
+ * 
  */
 public class Allocate {
 	private static final int _1MB = 1024 * 1024;
@@ -24,7 +35,13 @@ public class Allocate {
 		allocation1 = new byte[2 * _1MB];
 		allocation2 = new byte[2 * _1MB];
 		allocation3 = new byte[2 * _1MB];
+		System.gc();
 		allocation4 = new byte[4 * _1MB]; // 出现一次Minor GC
+		allocation1 = new byte[2 * _1MB];
+		System.gc();
+		allocation1 = new byte[2 * _1MB];
+		allocation1 = new byte[2 * _1MB];
+//		System.gc();
 	}
 
 	public static void main(String[] args) {
